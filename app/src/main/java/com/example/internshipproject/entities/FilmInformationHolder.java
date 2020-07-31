69b164f652662d7c8a4a5ac86faf6f649f062c76
Yaroslav Lichnyi <yaroslav.lichnyi@gmail.com>
2020-07-31 11:41:19 +0300

    Class-wrapper was added

---
+++ app/src/main/java/com/example/internshipproject/entities/FilmInformationHolder.java
@@ -0,0 +1,27 @@
+package com.example.internshipproject.entities;
+
+public class FilmInformationHolder {
+    private Film film;
+    private FilmDetails filmDetails;
+
+    public FilmInformationHolder(Film film, FilmDetails filmDetails) {
+        this.film = film;
+        this.filmDetails = filmDetails;
+    }
+
+    public Film getFilm() {
+        return film;
+    }
+
+    public void setFilm(Film film) {
+        this.film = film;
+    }
+
+    public FilmDetails getFilmDetails() {
+        return filmDetails;
+    }
+
+    public void setFilmDetails(FilmDetails filmDetails) {
+        this.filmDetails = filmDetails;
+    }
+}
